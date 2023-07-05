93df5c2c32d538ecba76193731e19b249b153f70
gustavoatb <gustavoatb76@gmail.com>
2023-07-04 23:32:39 -0300

    Atividade-09

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
 
 const routes: Routes = [
-  //{path: 'home', component: HomeComponent},
-  //{path: 'contato', component: ContatoComponet},
+  {path: 'home', component: HomeComponent},
+  {path: 'contato', component: ContatoComponent},
 ];
 
 @NgModule({
