6805c4b7b8189ec312ca054b94c6112939c4dcb8
gustavoatb <gustavoatb76@gmail.com>
2023-07-27 01:15:40 -0300

    NovasFuncionalidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { ListaPessoasComponent } from './lista-pessoas/lista-pessoas.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,9 +15,11 @@ import { ListaPessoasComponent } from './lista-pessoas/lista-pessoas.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
